3c4ebb040479d218a2e3163ca78c4bb5e1a3d247
eriko-creator <koenuma.eri@gmail.com>
2021-01-06 17:48:28 +0900

    Initial Commit

---
+++ NavitimeApiPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavitimeApiPractice
+//
+//  Created by 肥沼英里 on 2021/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
