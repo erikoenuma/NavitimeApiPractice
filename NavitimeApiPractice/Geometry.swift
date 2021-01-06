2a568be1ef3d111724ab3583567764a6e2cb72be
eriko-creator <koenuma.eri@gmail.com>
2021-01-07 03:39:06 +0900

    feat: 出発・到着地点名を取得する機能を追加

--- NavitimeApiPractice/Geometry.swift
+++ NavitimeApiPractice/Geometry.swift
@@ -13,10 +13,24 @@ struct Geometry: Codable{
 
 struct results: Codable{
     let geometry : geometry
+    let address: [Address]
+    
+    enum CodingKeys: String, CodingKey{
+        case geometry
+        case address = "address_components"
+    }
 }
 
 struct geometry: Codable{
     let location: location
+}
+
+struct Address:Codable{
+    let shortName: String
+    
+    enum CodingKeys: String,CodingKey{
+        case shortName = "short_name"
+    }
 }
 
 struct location: Codable{
