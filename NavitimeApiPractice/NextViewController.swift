03000db57f8fff52717f8fcd20b3416c5e7853b4
Eriko-creator <koenuma.eri@gmail.com>
2021-03-29 23:28:31 +0900

    コメントアウトしていた不要なコードを削除

--- NavitimeApiPractice/NextViewController.swift
+++ NavitimeApiPractice/NextViewController.swift
@@ -56,8 +56,6 @@ class NextViewController: UIViewController {
                         
                         let baseUrl = "https://navitime-route-car.p.rapidapi.com/route_car"
                         
-                        //?start=35.689457,139.691935&goal=00007423&start_time=2019-10-01T08:00:00&options=turn_by_turn"
-                        
                         guard var components = URLComponents(string: baseUrl), let startResults = start.results.first, let goalResults = goal.results.first else {return}
                         
                         components.queryItems = [
@@ -90,43 +88,4 @@ class NextViewController: UIViewController {
                     }
         }
     }
-        
-        
-//        let headerContent = [
-//            "x-rapidapi-host": "navitime-route-car.p.rapidapi.com",
-//            "x-rapidapi-key": "d058e7a572msh65b2857f9c8bdf7p1f1fe3jsne067d62882cb"
-//        ]
-//
-//        let headers = HTTPHeaders.init(headerContent)
-//
-//        let baseUrl = "https://navitime-route-car.p.rapidapi.com/route_car"
-//
-//        //?start=35.689457,139.691935&goal=00007423&start_time=2019-10-01T08:00:00&options=turn_by_turn"
-//
-//        guard var components = URLComponents(string: baseUrl) else {return}
-//
-//        components.queryItems = [
-//
-//            URLQueryItem(name: "start", value: startPosition),
-//            URLQueryItem(name: "goal", value: goalPosition),
-//            URLQueryItem(name: "options", value: "turn_by_turn"),
-//            URLQueryItem(name: "start_time", value: "2021-1-4T10:00:00")
-//
-//        ]
-//
-//        guard let url = components.url else {return}
-//        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
-//
-//            switch response.result{
-//
-//            case .success:
-//                let json:JSON = JSON(response.data as Any)
-//                print(json)
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-//
-//    }
-
 }
